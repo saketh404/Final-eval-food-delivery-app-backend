58e0a17e0fc3c671b4c8f5d5b2a5e0f7558219a1
saketh404 <saketh404@gmail.com>
2024-11-24 12:51:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv");
+env.config();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running on port 3000");
+});
